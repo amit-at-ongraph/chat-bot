1960f0f0b4d0f44a9732251e5134de56cd755100
amit-at-ongraph <amit.k@ongraph.com>
2026-01-29 14:34:03 +0530

    Refactor: Externalize database connection details using environment variables in Drizzle config and DB client.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,15 +5,10 @@ export default defineConfig({
   out: "./lib/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    database: "postgres",
-    user: "postgres.nydoiezsepvcautphznl",
-    password: "m11vzDqLhBX4QeEm",
-    host: "aws-1-ap-south-1.pooler.supabase.com",
-    port: 5432,
-    // ssl: true,
-    // url: "postgresql://postgres.nydoiezsepvcautphznl:m11vzDqLhBX4QeEm@aws-1-ap-south-1.pooler.supabase.com:6543/postgres",
+    database: process.env.DB_NAME!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT!),
   },
-
-  // This prevents Drizzle from trying to 'read' Supabase system schemas
-  // schemaFilter: ["public"],
 });
--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,8 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const connectionString =
-  "postgresql://postgres.nydoiezsepvcautphznl:m11vzDqLhBX4QeEm@aws-1-ap-south-1.pooler.supabase.com:5432/postgres";
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 // Disable prefetch as it is not supported for "Transaction" mode
 export const client = postgres(connectionString, { prepare: false });
