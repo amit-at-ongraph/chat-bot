bfe0d336c26eed1a16eed8b032c296777db02adb
amit-at-ongraph <amit.k@ongraph.com>
2026-02-06 15:46:41 +0530

    feat: Set toast notification duration to 2000ms.

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -9,7 +9,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     <SessionProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         {children}
-        <Toaster richColors position="top-right" />
+        <Toaster richColors position="top-right" duration={2000} />
       </ThemeProvider>
     </SessionProvider>
   );
